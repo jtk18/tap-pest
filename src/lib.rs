fc845e3999aa3e8c247703cff7eac8d090a3e821
Jason Kiniry <jason.kiniry@gmail.com>
2021-03-21 22:10:11 -0500

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,3 @@ pub mod tap;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
-
--- src/tap.rs
+++ src/tap.rs
@@ -7,19 +7,18 @@ mod tests {
     use super::*;
     use pest::{consumes_to, fails_with, parses_to};
 
+    //     #[test]
+    //     fn test_tap_from_website() {
+    //         let tap = r#"1..4
+    // ok 1 - Input file opened
+    // not ok 2 - First line of the input valid
+    // ok 3 - Read the rest of the file
+    // not ok 4 - Summarized correctly # TODO Not written yet"#;
 
-//     #[test]
-//     fn test_tap_from_website() {
-//         let tap = r#"1..4
-// ok 1 - Input file opened
-// not ok 2 - First line of the input valid
-// ok 3 - Read the rest of the file
-// not ok 4 - Summarized correctly # TODO Not written yet"#;
+    //         let out = TapParser::parse(Rule::tap, tap);
 
-//         let out = TapParser::parse(Rule::tap, tap);
-
-//         println!("{:#?}", out);
-//     }
+    //         println!("{:#?}", out);
+    //     }
 
     #[test]
     fn test_tap_non_newline() {
